191d7a758bee7db1daeaad21f914810b46671d83
Emil Sit <sit@emilsit.net>
2013-07-16 16:26:28 -0400

    HACK: Connect stdout/stderr to shutdown command
    Trying to debug a shutdown command that doesn't actually shut down.

--- builder/virtualbox/step_shutdown.go
+++ builder/virtualbox/step_shutdown.go
@@ -3,9 +3,12 @@ package virtualbox
 import (
 	"errors"
 	"fmt"
+	"github.com/mitchellh/iochan"
 	"github.com/mitchellh/multistep"
 	"github.com/mitchellh/packer/packer"
+	"io"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -32,8 +35,17 @@ func (s *stepShutdown) Run(state map[string]interface{}) multistep.StepAction {
 
 	if config.ShutdownCommand != "" {
 		ui.Say("Gracefully halting virtual machine...")
-		log.Printf("Executing shutdown command: %s", config.ShutdownCommand)
+
+		// Setup the remote command
+		stdout_r, stdout_w := io.Pipe()
+		stderr_r, stderr_w := io.Pipe()
+
 		cmd := &packer.RemoteCmd{Command: config.ShutdownCommand}
+
+		cmd.Stdout = stdout_w
+		cmd.Stderr = stderr_w
+
+		log.Printf("Executing shutdown command: %s", cmd.Command)
 		if err := comm.Start(cmd); err != nil {
 			err := fmt.Errorf("Failed to send shutdown command: %s", err)
 			state["error"] = err
@@ -41,12 +53,53 @@ func (s *stepShutdown) Run(state map[string]interface{}) multistep.StepAction {
 			return multistep.ActionHalt
 		}
 
-		// Wait for the command to run
-		cmd.Wait()
+		exitChan := make(chan int, 1)
+		stdoutChan := iochan.DelimReader(stdout_r, '\n')
+		stderrChan := iochan.DelimReader(stderr_r, '\n')
 
 		// Wait for the machine to actually shut down
 		log.Printf("Waiting max %s for shutdown to complete", config.shutdownTimeout)
 		shutdownTimer := time.After(config.shutdownTimeout)
+
+		go func() {
+			defer stdout_w.Close()
+			defer stderr_w.Close()
+
+			cmd.Wait()
+			exitChan <- cmd.ExitStatus
+		}()
+
+	OutputLoop:
+		for {
+			select {
+			case output := <-stderrChan:
+				ui.Message(strings.TrimSpace(output))
+			case output := <-stdoutChan:
+				ui.Message(strings.TrimSpace(output))
+			case exitStatus := <-exitChan:
+				log.Printf("shutdown command exited with status %d", exitStatus)
+
+				if exitStatus != 0 {
+                                        err := fmt.Errorf("shutdown command exited with non-zero exit status: %d", exitStatus)
+                                        state["error"] = err
+                                        ui.Error(err.Error())
+					return multistep.ActionHalt
+				}
+
+				break OutputLoop
+			}
+		}
+
+		// Make sure we finish off stdout/stderr because we may have gotten
+		// a message from the exit channel first.
+		for output := range stdoutChan {
+			ui.Message(output)
+		}
+
+		for output := range stderrChan {
+			ui.Message(output)
+		}
+
 		for {
 			running, _ := driver.IsRunning(vmName)
 			if !running {
